fc1edd11b8c303c2bbcd56c74713b60aea8442de
spksoft <spkrsk.37@gmail.com>
2021-08-22 05:11:40 +0700

    add comment

--- pipeline.go
+++ pipeline.go
@@ -3,19 +3,23 @@ package pipeline
 // Processor is a function type for execute input and return output
 type Processor func(input <-chan interface{}) <-chan interface{}
 
+// Pipeline is a pipeline of processors
 type Pipeline struct {
 	registedProcessors []Processor
 }
 
+// IPipeline is an interface for pipeline
 type IPipeline interface {
 	RegisterProcessor(processor Processor)
 	Run() (result <-chan interface{})
 }
 
+// RegisterProcessor registers a processor
 func (p *Pipeline) RegisterProcessor(processor Processor) {
 	p.registedProcessors = append(p.registedProcessors, processor)
 }
 
+// Run runs the pipeline
 func (p *Pipeline) Run() (result <-chan interface{}) {
 	nextInput := make(<-chan interface{})
 	for _, processor := range p.registedProcessors {
@@ -24,6 +28,7 @@ func (p *Pipeline) Run() (result <-chan interface{}) {
 	return nextInput
 }
 
+// New returns a new pipeline
 func New() *Pipeline {
 	p := Pipeline{}
 	return &p
